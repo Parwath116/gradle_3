cebef57372ae82e35e21877313662436112f9bf7
Parwath116 <parwathsrirajuk@gmail.com>
2026-06-19 07:18:10 +0530

    adding all index, style files for deployment

---
+++ src/test/java/org/WebpageTest/WebpageTest.java
@@ -0,0 +1,31 @@
+package org.WebpageTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+public class WebpageTest {
+    private static WebDriver driver;
+    @BeforeTest
+    public void openBrowser() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        driver.get("https://github.com/Parwath116/gradle_3.git");
+    }
+    @Test
+    public void titleValidationTest(){
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Parwath Sri Raju K,BIT,ICB";
+        Assert.assertEquals(actualTitle, expectedTitle);
+        assertTrue(true, "Title should contain 'Praveen'");
+    }
+    @AfterTest
+    public void closeBrowser() throws InterruptedException {
+        Thread.sleep(10000);
+        driver.quit();
+    }
+}
